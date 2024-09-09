317d3b73f9b76da742ccec8d22761ad43506dc71
Jagadeesh <jagadeesh28.m@gmail.com>
2024-09-09 10:34:26 +0530

    Added inputs and button compnents

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { CreateTodo } from "./components/CreateTodo";
+
+function App() {
+  return (
+    <>
+      <CreateTodo></CreateTodo>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,13 @@
+export function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder="title" />
+      <br />
+      <br />
+      <input type="text" placeholder="description" />
+      <br />
+      <br />
+      <button>Add Todo</button>
+    </div>
+  );
+}
